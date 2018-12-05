9b43816bcb303267026c48dae1dad50c6c7daee5
Vladislav <zvvlad2@gmail.com>
2018-12-05 18:23:29 +0600

    clear

--- practicefour/src/main/java/com/sevenbits/practicefour/UniqueStorage/IUniqueStorage.java
+++
@@ -1,9 +0,0 @@
-package com.sevenbits.practicefour.UniqueStorage;
-
-import com.sevenbits.practicefour.Planet;
-
-public interface IUniqueStorage {
-    void add(Planet planet);
-    void remove(Planet planet);
-    boolean contains(Planet planet);
-}
